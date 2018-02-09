29ba3d1d217a2f1fb59081a00ddba943ce8e557d
Luke Carrier <luke@carrier.im>
2018-02-09 18:34:52 +0000

    Add bootstrap module for including all JS components

---
+++ amd/src/bootstrap.js
@@ -0,0 +1,15 @@
+define([
+    'floreamui_bootstrap/affix',
+    'floreamui_bootstrap/alert',
+    'floreamui_bootstrap/bootstrap',
+    'floreamui_bootstrap/button',
+    'floreamui_bootstrap/carousel',
+    'floreamui_bootstrap/collapse',
+    'floreamui_bootstrap/dropdown',
+    'floreamui_bootstrap/modal',
+    'floreamui_bootstrap/popover',
+    'floreamui_bootstrap/scrollspy',
+    'floreamui_bootstrap/tab',
+    'floreamui_bootstrap/tooltip',
+    'floreamui_bootstrap/transition'
+], function() {});
